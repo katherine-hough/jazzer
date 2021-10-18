b32b0644aa4c32c464f8488170b0e812dec998fb
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2021-10-18 12:27:04 +0200

    Pass arguments to jazzer in jazzer-autofuzz
    Also reorder directives for better caching.

--- docker/jazzer-autofuzz/entrypoint.sh
+++ docker/jazzer-autofuzz/entrypoint.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 CP="$(/app/coursier.jar fetch --classpath "$1")"
 /app/jazzer_driver \
   -artifact_prefix=/fuzzing/ \
   --reproducer_path=/fuzzing \
   --cp="$CP" \
-  --autofuzz="$2"
+  --autofuzz="$2" \
+  "${@:3}"
